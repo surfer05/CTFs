676ed5d13368c4d45ff273227c41bf9d2aab7c0d
Alok Kumar <rahul05alok@gmail.com>
2023-12-21 20:58:28 +0530

    Still on Elevator, will get to the gatekeeper

--- ethernaut/script/Counter.s.sol
+++ ethernaut/script/Counter.s.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.13;
 
 import {Script, console2} from "forge-std/Script.sol";
-import {Attack, Token} from "../src/Token/token.sol";
+import {Attack, Telephone} from "../src/Telephone/telephone.sol";
 
 contract CounterScript is Script {
     function setUp() public {}
@@ -10,7 +10,7 @@ contract CounterScript is Script {
     function run() public returns (Attack) {
         vm.startBroadcast();
         Attack swi = new Attack(
-            Token(0x795803DEa61B7cd15ECBb3CFFAbbf59395Eaaa75)
+            Telephone(0x795803DEa61B7cd15ECBb3CFFAbbf59395Eaaa75)
         );
         vm.stopBroadcast();
         return swi;
